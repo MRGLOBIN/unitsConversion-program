c89cec5cfc417315d4196bfaa2a067ec01d938b0
Muhammad Asim <asimqwe2@gmail.com>
2023-10-14 16:51:14 +0500

    changes in cout string

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <format>
 
 using namespace std;
 
-const string prefix = {};
+const string prefix[] = {"Micro", "Milli", "centi", "Kilo", "Mega"};
+const string units[] = {"Gram"};
 
 int main()
 {
@@ -13,12 +15,29 @@ int main()
         cout << "Please select apporiate unit for conversion: \n";
         cout << "1.Mass\n2.Time\n3.Length\n4.Temperature\n5.Current\n6.Intensity of light\n7.Amount of substance\n";
         cin >> option;
-    } while (option < 0 || option > 8);
+        switch (option)
+        {
 
-    if (option == 1)
-    {
-        cout << "from what value do you want to convert: ";
-        cout << "1.Micro-Gram\n2.Milli-Gram\n3.Centi-Gram\n4.Gram\n5.Kilo-Gram";
-        cin >> option;
-    }
+        case 1:
+        {
+            cout << "from what value do you want to convert: ";
+            cout << "\n1." << prefix[0] << "-" << units[0] << "\n2." << prefix[1]
+                 << "-" << units[0] << "\n3." << prefix[2] << "-" << units[0] << "\n4." << units[0] << "\n5." << prefix[4] << "-" << units[0] << "";
+            cin >> option;
+        }
+        break;
+        case 2:
+        {
+            cout << "from what value do you want to convert: ";
+            cout << "\n1." << prefix[0] << "-Gram\n2." << prefix[1]
+                 << "-Gram\n3." << prefix[2] << "-Gram\n4.Gram\n5." << prefix[4] << "-Gram";
+            cin >> option;
+        }
+
+        default:
+        {
+            cout << "Please select correct option\n\n";
+        }
+        }
+    } while (option > 7 || option < 1);
 }
